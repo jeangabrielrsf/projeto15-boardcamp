46b1d5ef4101a5d0defd75c88bd35e4a733b2b1a
Jean Frusca <jeang96@gmail.com>
2022-09-30 22:16:06 -0300

    feat: rota GET de categorias criada.

--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,13 @@
+import pg from "pg";
+
+const { Pool } = pg;
+
 const connection = new Pool({
-	connectionString: process.env.DATABASE_URL,
+	host: "localhost",
+	port: 5432,
+	user: "postgres",
+	password: "narutin",
+	database: "boardcamp",
 });
+
+export default connection;
--- src/server.js
+++ src/server.js
@@ -1,8 +1,22 @@
 import express from "express";
 import cors from "cors";
+import connection from "./database/database.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.get("/categories", async (req, res) => {
+	try {
+		const categories = await connection.query(`
+        SELECT * FROM categories;
+    `);
+
+		res.send(categories.rows);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
 
 app.listen(4000, () => console.log("Listening on port 4000..."));
