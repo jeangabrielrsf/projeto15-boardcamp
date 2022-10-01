0e37e87659b27d8ff984feef3727c132ea9a0e53
Jean Frusca <jeang96@gmail.com>
2022-09-30 22:27:22 -0300

    feat: rota POST de categorias criada.

--- src/server.js
+++ src/server.js
@@ -19,4 +19,38 @@ app.get("/categories", async (req, res) => {
 	}
 });
 
+app.post("/categories", async (req, res) => {
+	try {
+		const { name } = req.body;
+
+		if (!name) {
+			return res.sendStatus(400);
+		}
+
+		const nameCheck = await connection.query(
+			`
+            SELECT name FROM categories WHERE categories.name = $1;
+        `,
+			[name]
+		);
+
+		console.log(nameCheck.rows);
+		if (nameCheck.rowCount != 0) {
+			return res.sendStatus(409);
+		}
+
+		connection.query(
+			`
+            INSERT INTO categories (name) VALUES ($1);
+        `,
+			[name]
+		);
+
+		res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
+
 app.listen(4000, () => console.log("Listening on port 4000..."));
