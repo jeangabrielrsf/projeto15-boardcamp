80aa4140298179946eb7ab18dcbb156b6c51df84
Jean Frusca <jeang96@gmail.com>
2022-10-03 11:18:05 -0300

    Configurado dotenv para conexão com banco de dados.

--- src/database/database.js
+++ src/database/database.js
@@ -1,13 +1,11 @@
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const { Pool } = pg;
 
 const connection = new Pool({
-	host: "localhost",
-	port: 5432,
-	user: "postgres",
-	password: "narutin",
-	database: "boardcamp",
-});
+	connectionString: process.env.DATABASE_URL,
+  });
 
 export default connection;
