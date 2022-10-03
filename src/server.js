7ff47fee45beb2d27c8ac29bf3f8a03382806e3c
Jean Frusca <jeang96@gmail.com>
2022-10-02 21:15:48 -0300

    feat: rota POST de jogos criada.

--- src/server.js
+++ src/server.js
@@ -66,4 +66,50 @@ app.get("/games", async (req, res) => {
 	}
 });
 
+app.post("/games", async (req, res) => {
+	try {
+		const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+		const categoryCheck = await connection.query(
+			`
+			SELECT * FROM categories WHERE categories.id = $1; 
+		`,
+			[categoryId]
+		);
+
+		if (
+			name.length === 0 ||
+			stockTotal === 0 ||
+			pricePerDay === 0 ||
+			categoryCheck.rowCount === 0
+		) {
+			return res.sendStatus(400);
+		}
+
+		const nameCheck = await connection.query(
+			`
+            SELECT name FROM games WHERE games.name = $1;
+        `,
+			[name]
+		);
+
+		if (nameCheck.rowCount != 0) {
+			return res.sendStatus(409);
+		}
+
+		connection.query(
+			`
+		 INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay")
+		 VALUES ($1, $2, $3, $4, $5);
+		`,
+			[name, image, stockTotal, categoryId, pricePerDay]
+		);
+
+		return res.sendStatus(201);
+	} catch (error) {
+		console.log(error);
+		return res.sendStatus(500);
+	}
+});
+
 app.listen(4000, () => console.log("Listening on port 4000..."));
